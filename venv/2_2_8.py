98c23d08dfc0ac928125f108a9354705bcb23925
Flash <flashikk@inbox.ru>
2021-08-18 19:17:52 +0500

    add dz 2_2_8 добавляем файл для загрузки

---
+++ venv/2_2_8.py
@@ -0,0 +1,27 @@
+'''
+Загрузка файла:
+1. Открыть страницу http://suninjuly.github.io/file_input.html
+2. Заполнить текстовые поля: имя, фамилия, email
+3. Загрузить файл. Файл должен иметь расширение .txt и может быть пустым
+4. Нажать кнопку "Submit"
+'''
+import os, time
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\Py_Projects\chromedriver\chromedriver.exe")
+driver.get("http://suninjuly.github.io/file_input.html")
+
+#Заполняем текстовые поля (ищем по значению атрибута name)
+driver.find_element_by_name('firstname').send_keys('Игорь')
+driver.find_element_by_name('lastname').send_keys('Насыров')
+driver.find_element_by_name('email').send_keys('flashikk@inbox.ru')
+
+#Получаем путь к директории текущего исполняемого файла
+current_dir = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(current_dir, 'test.txt') #добавляем к этому пути имя файла
+
+#Находим кнопку и передаем ей путь к файлу file_path
+driver.find_element_by_name('file').send_keys(file_path)
+
+#Нажимаем кнопку Submit
+driver.find_element_by_css_selector('.btn-primary').click()
